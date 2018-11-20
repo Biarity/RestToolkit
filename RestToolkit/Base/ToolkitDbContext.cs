9872a6e591bce88225975d4180f2629df4fe7d40
Biarity <biarity@outlook.com>
2018-11-20 13:41:23 +1000

    seed users

--- RestToolkit/Base/ToolkitDbContext.cs
+++ RestToolkit/Base/ToolkitDbContext.cs
@@ -8,5 +8,21 @@ namespace RestToolkit.Base
         where TUser : ToolkitUser
     {
         public ToolkitDbContext(DbContextOptions options) : base(options) { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            SeedUsers(modelBuilder);
+        }
+
+        protected virtual void SeedUsers(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TUser>().HasData(
+                new { Id = 0, UserName = "dev", Email = "biarity@outlook.com" },
+                new { Id = 1, UserName = "dev2", Email = "example@example.com" },
+                new { Id = 2, UserName = "dev3", Email = "example@example.com" });
+        }
     }
 }
